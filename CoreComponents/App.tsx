b62d25fceec4b7d9740c48486fd4378cefeba007
Praveen Singh <Praveencdg@gmail.com>
2025-02-03 23:06:42 +0530

    Pressable

--- CoreComponents/App.tsx
+++ CoreComponents/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Button } from "react-native";
-
+import { View, Button, Image, Text, Pressable } from "react-native";
+import LogoImg from "./assets/splash-icon.png";
 export default function App() {
   return (
     <View style={{ flex: 1, paddingTop: 45, paddingHorizontal: 10 }}>
@@ -10,6 +10,26 @@ export default function App() {
         color="midnightblue"
         disabled
       />
+      <Pressable
+        onPress={() => console.log("Images Pressed")}
+        onPressOut={() => console.log("Onpress Out")}
+        onLongPress={() => console.log("Holded for 500 millisecond")}
+      >
+        <Image
+          source={LogoImg}
+          style={{ height: 300, width: 300, paddingVertical: 10 }}
+        />
+      </Pressable>
+      <Pressable onPress={() => console.log("Text Pressed")}>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          itaque labore optio laboriosam, tempora exercitationem rem. Culpa
+          quae, ea at dolor assumenda consequatur corporis quaerat voluptate,
+          corrupti iste, similique voluptates omnis voluptatibus recusandae sit
+          distinctio rerum! Sunt cum eos nostrum rerum saepe, dolorem debitis
+          dolorum quasi amet ad rem iure.
+        </Text>
+      </Pressable>
       <StatusBar />
     </View>
   );
