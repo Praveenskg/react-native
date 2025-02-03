72063d781f4a0c359bc25c74e2be20624759c143
Praveen Singh <Praveencdg@gmail.com>
2025-02-03 22:36:56 +0530

    checkout  view, image and  image background

--- CoreComponents/App.tsx
+++ CoreComponents/App.tsx
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import ImageLogo from "./assets/adaptive-icon.png";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      {/*  Image with inLine Style */}
+      {/* <Image source={ImageLogo} style={{ width: 300, height: 300 }} /> */}
+      {/* Image with Url  */}
+      {/* <Image
+        source={{ uri: "https://picsum.photos/300" }}
+        style={{ height: 300, width: 300 }}
+      /> */}
+      <ImageBackground
+        source={{ uri: "https://picsum.photos/300" }}
+        style={{ flex: 1 }}
+      >
+        <Text> Hello World </Text>
+      </ImageBackground>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 30,
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  text: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
   },
 });
