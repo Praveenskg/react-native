d1ed842c55eeb4027bea74821b76155e970a6e8e
Praveen Singh <Praveencdg@gmail.com>
2025-02-04 19:38:40 +0530

    Flex Wrap

--- RNLayout/App.tsx
+++ RNLayout/App.tsx
@@ -4,19 +4,13 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#FF5733", alignSelf: "flex-start" }}>
-        Box 1
-      </Box>
-      <Box style={{ backgroundColor: "#33FF57", alignSelf: "flex-end" }}>
-        Box 2
-      </Box>
-      <Box style={{ backgroundColor: "#3357FF", alignSelf: "center" }}>
-        Box 3
-      </Box>
-      <Box style={{ backgroundColor: "#FF33A8", alignSelf: "stretch" }}>
-        Box 4
-      </Box>
-      <Box style={{ backgroundColor: "#FFD700", alignSelf:"auto" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "#FF5733" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#33FF57" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#3357FF" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "#FF33A8" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#FFD700" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "#FF33A8" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#FFD700" }}>Box 5</Box>
     </View>
     // <View style={{ backgroundColor: "plum", flex: 1 }}> </View>
   );
@@ -25,9 +19,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems:"stretch",
+    flexDirection: "row",
+    flexWrap: "wrap-reverse",
+    // height: 300,
     marginTop: 64,
     borderWidth: 4,
-    borderColor: "red",
+    borderColor: "green",
   },
 });
