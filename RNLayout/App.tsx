48d74438e9bc563b805a1ac9be6584657a99bb0d
Praveen Singh <Praveencdg@gmail.com>
2025-02-04 15:38:52 +0530

    Flex

--- RNLayout/App.tsx
+++ RNLayout/App.tsx
@@ -4,18 +4,20 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#FF5733" }}>Box 1</Box>
-      <Box style={{ backgroundColor: "#33FF57" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#FF5733", flex: 1 }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#33FF57", flex: 3 }}>Box 2</Box>
       <Box style={{ backgroundColor: "#3357FF" }}>Box 3</Box>
       <Box style={{ backgroundColor: "#FF33A8" }}>Box 4</Box>
       <Box style={{ backgroundColor: "#FFD700" }}>Box 5</Box>
     </View>
+    // <View style={{ backgroundColor: "plum", flex: 1 }}> </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     marginTop: 64,
+    flex: 1,
     borderWidth: 4,
     borderColor: "red",
   },
