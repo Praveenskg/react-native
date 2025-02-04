b5d0e764b984ff57ca0330651924429e05163832
Praveen Singh <Praveencdg@gmail.com>
2025-02-04 15:31:26 +0530

    FlexBox

--- RNLayout/App.tsx
+++ RNLayout/App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import Box from "./components/Box";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Box style={{ backgroundColor: "#FF5733" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#33FF57" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#3357FF" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "#FF33A8" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#FFD700" }}>Box 5</Box>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 64,
+    borderWidth: 4,
+    borderColor: "red",
   },
 });
---
+++ RNLayout/components/Box.tsx
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet, ViewStyle, StyleProp } from "react-native";
+
+type BoxProps = {
+  children: string;
+  style?: StyleProp<ViewStyle>;
+};
+export default function Box({ children, style }: BoxProps) {
+  return (
+    <View style={[styles.box, style]}>
+      <Text style={styles.text}>{children} </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: "white",
+    padding: 20,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+  },
+});
