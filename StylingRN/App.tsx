355534c63ed16c54a4d77b0a833a092f7c9baeb4
Praveen Singh <Praveencdg@gmail.com>
2025-02-04 14:30:12 +0530

    Multiple Styles

--- StylingRN/App.tsx
+++ StylingRN/App.tsx
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={[styles.box, styles.lightblueBg]}>
+        <Text>Lightblue Box</Text>
+      </View>
+      <View style={[styles.box, styles.lightGreenBg]}>
+        <Text>LightGreen Box</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,5 +20,16 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 60,
     backgroundColor: "#fff",
+  },
+  box: {
+    width: 100,
+    height: 100,
+    padding: 10,
+  },
+  lightblueBg: {
+    backgroundColor: "lightblue",
+  },
+  lightGreenBg: {
+    backgroundColor: "lightgreen",
   },
 });
