172788e4bd4c928d403e2888eee8dcafea305dfe
Praveen Singh <Praveencdg@gmail.com>
2025-02-04 16:03:11 +0530

    Align Items

--- RNLayout/App.tsx
+++ RNLayout/App.tsx
@@ -4,7 +4,9 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#FF5733" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#FF5733", paddingVertical: 100 }}>
+        Box 1
+      </Box>
       <Box style={{ backgroundColor: "#33FF57" }}>Box 2</Box>
       <Box style={{ backgroundColor: "#3357FF" }}>Box 3</Box>
       {/* <Box style={{ backgroundColor: "#FF33A8" }}>Box 4</Box>
@@ -18,7 +20,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "row",
-    justifyContent: "center",
+    alignItems: "baseline",
     marginTop: 64,
     borderWidth: 4,
     borderColor: "red",
